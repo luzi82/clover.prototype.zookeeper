dfa413550766026e17cb31999cb06be15bcdf222
luzi82 <luzi82@gmail.com>
2017-08-04 18:15:21 +0800

    working main_menu

--- clover/zookeeper/state_list/main_menu.py
+++ clover/zookeeper/state_list/main_menu.py
@@ -1,6 +1,7 @@
 from zookeeper_screen_recognition import classifier_main_menu_button
 from clover.common import draw_util
 import os
+import sys
 
 BUTTON_XY = (166+(308/2), 174+(188/2))
 NEXT_ITEM_BUTTON_XY = (551+(60/2), 341+(54/2))
@@ -14,25 +15,33 @@ def tick(bot_logic, img, arm, t, ret):
     rret = ret['main_menu_data']
 
     if t < bot_logic.main_menu_cooldown:
+        # print('IVRWEGUZPS t < bot_logic.main_menu_cooldown',file=sys.stderr)
         return False
-    if arm and (arm['is_moving']):
+    if arm and (arm['is_busy']):
+        # print('PVQYDYNBCE is_busy',file=sys.stderr)
         return False
 
     button_label, _ = bot_logic.main_menu_button_clr.predict(img)
     rret['button_label'] = button_label
 
-    if arm:
-        if button_label == 'a_practice':
-            target_xy = BUTTON_XY
-        else:
-            target_xy = NEXT_ITEM_BUTTON_XY
+    target_xy = None
+
+    if not arm:
+        pass
+    elif button_label == 'a_practice':
+        #target_xy = BUTTON_XY
+        pass
+    elif button_label[:2] == 'a_':
+        target_xy = NEXT_ITEM_BUTTON_XY
+
+    if target_xy:
         ret['arm'] = [
             (arm['xyz'][:2])+(0,),
             target_xy+(0,),
             target_xy+(1,),
             target_xy+(0,)
         ]
-    bot_logic.main_menu_cooldown = t + 1
+        bot_logic.main_menu_cooldown = t + 3
 
     return True
 
