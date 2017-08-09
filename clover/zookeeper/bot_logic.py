26829c2d15f065c23f10ea1e38c97353a401de85
luzi82 <luzi82@gmail.com>
2017-08-09 18:46:23 +0800

    add pause and screen cap btn

--- clover/zookeeper/bot_logic.py
+++ clover/zookeeper/bot_logic.py
@@ -2,8 +2,11 @@ import numpy as np
 import pygame
 import os
 import json
-from clover.common import draw_util
+import random
+import time
+import cv2
 
+from clover.common import draw_util
 from zookeeper_screen_recognition import classifier_state
 from clover.zookeeper.state_list import battle
 from clover.zookeeper.state_list import battle_result
@@ -18,9 +21,6 @@ from clover import common
 SCREEN_SIZE = bot.SCREEN_SIZE
 STATE_CLR_MODEL_PATH = os.path.join('dependency','zookeeper_screen_recognition','model')
 
-BTN_SIZE = 30
-PLAY_BTN_RECT = (SCREEN_SIZE[0]-BTN_SIZE,SCREEN_SIZE[1]-BTN_SIZE,SCREEN_SIZE[0],SCREEN_SIZE[1])
-
 class BotLogic:
 
     def __init__(self):
@@ -33,19 +33,36 @@ class BotLogic:
         self.state_op_dict['title'] = title
 
         self.play = False
+        self.cap_screen = False
+        
+        self.cap_screen_timeout = 0
+        self.cap_screen_output_folder = os.path.join('dependency','zookeeper_screen_recognition','raw_image',str(int(time.time()*1000)))
 
     def init(self):
         self.state_clr = classifier_state.StateClassifier(STATE_CLR_MODEL_PATH)
         for _, state_op in self.state_op_dict.items():
             state_op.init(self)
+        z_pause.init(self)
 
     def on_event(self,event):
         if event.type == pygame.MOUSEBUTTONUP:
             pos = pygame.mouse.get_pos()
             if common.in_rect(pos,PLAY_BTN_RECT):
                 self.play = not self.play
+            if common.in_rect(pos,SCREENCAP_BTN_RECT):
+                self.cap_screen = not self.cap_screen
 
     def tick(self,img,arm_data,time_s):
+        if self.cap_screen:
+            if time_s >= self.cap_screen_timeout:
+                t = int(time_s*1000)
+                t0 = int(t/100000)
+                fn_dir = os.path.join(self.cap_screen_output_folder,str(t0))
+                common.makedirs(fn_dir)
+                fn = os.path.join(fn_dir,'{}.png'.format(t))
+                cv2.imwrite(fn,img)
+                self.cap_screen_timeout = time_s + 0.5 + 0.5*random.random()
+
         img = img.astype('float32')*2/255-1
         
         state, _ = self.state_clr.get_state(img)
@@ -74,10 +91,16 @@ class BotLogic:
             if tick_result['play']:
                 if state in self.state_op_dict:
                     self.state_op_dict[state].draw(screen, tick_result)
+
         if self.play:
             screen.blit(draw_util.text('P',(0,127,0)), PLAY_BTN_RECT[:2])
         else:
             screen.blit(draw_util.text('S',(127,0,0)), PLAY_BTN_RECT[:2])
+
+        if self.cap_screen:
+            screen.blit(draw_util.text('CAP',(127,0,0)), SCREENCAP_BTN_RECT[:2])
+        else:
+            screen.blit(draw_util.text('NoCAP',(0,127,0)), SCREENCAP_BTN_RECT[:2])
 
 #    def render_state(self, screen, state):
 #        if not hasattr(self, 'state_render_dict'):
@@ -87,4 +110,10 @@ class BotLogic:
 #        if not state in self.state_render_dict:
 #            self.state_render_dict[state] = self.state_render_font.render(state, 1, (0,0,0))
 #        screen.blit(self.state_render_dict[state], (240,0))
-            
+
+
+BTN_SIZE = 30
+def btn_rect(idx):
+     return (SCREEN_SIZE[0]-(BTN_SIZE*(idx+1)),SCREEN_SIZE[1]-BTN_SIZE,SCREEN_SIZE[0]-(BTN_SIZE*idx),SCREEN_SIZE[1])
+PLAY_BTN_RECT = btn_rect(0)
+SCREENCAP_BTN_RECT = btn_rect(1)
--- clover/zookeeper/state_list/z_pause.py
+++ clover/zookeeper/state_list/z_pause.py
@@ -5,27 +5,27 @@ import sys
 BUTTON_XYZ = 320, -100, 0
 
 def init(bot_logic):
-    bot_logic.battle_result_cooldown_0 = 0
-    bot_logic.battle_result_cooldown_1 = 0
-    bot_logic.battle_result_cooldown_2 = 0
+    bot_logic.z_pause_cooldown_0 = 0
+    bot_logic.z_pause_cooldown_1 = 0
+    bot_logic.z_pause_cooldown_2 = 0
 
 def tick(bot_logic, img, arm, t, ret):
-    if t < bot_logic.battle_result_cooldown_0:
+    if t < bot_logic.z_pause_cooldown_0:
         return True
-    elif t < bot_logic.battle_result_cooldown_1:
+    elif t < bot_logic.z_pause_cooldown_1:
         ret['arm_move_list'] = [
             (arm['xyz'][:2])+(0,),
             BUTTON_XYZ
         ]
-        bot_logic.battle_result_cooldown_0 = 0
-        bot_logic.battle_result_cooldown_1 = 0
+        bot_logic.z_pause_cooldown_0 = 0
+        bot_logic.z_pause_cooldown_1 = 0
         return True
-    elif t < bot_logic.battle_result_cooldown_2:
+    elif t < bot_logic.z_pause_cooldown_2:
         return True
     else:
-        bot_logic.battle_result_cooldown_0 = t
-        bot_logic.battle_result_cooldown_1 = t+3
-        bot_logic.battle_result_cooldown_2 = t+4
+        bot_logic.z_pause_cooldown_0 = t
+        bot_logic.z_pause_cooldown_1 = t+3
+        bot_logic.z_pause_cooldown_2 = t+4
         return True
 
 def draw(screen, tick_result):
